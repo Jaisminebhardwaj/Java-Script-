66afd18434822edac3a1f96e0005bc2286956465
Jaisminebhardwaj <bhardwajjaismine@gmail.com>
2025-09-08 19:59:31 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,24 @@
+// Dates(months are 0-11 in JavaScript)
+let myDate = new Date();
+// console.log(myDate); 
+// console.log(myDate.toString()); 
+// console.log(myDate.toDateString()); 
+// console.log(myDate.getDay());
+// console.log(myDate.getMonth()); 
+// console.log(myDate.toJSON()); 
+// console.log(myDate.toLocaleString()); 
+// console.log(typeof(myDate)); //object
+
+// let newDate = new Date("2006-11-23");
+// let newDate = new Date("11/23/2006");
+
+// console.log(newDate.toLocaleString());
+
+//Timestamp (quizes , poll,fastest way to get time in js)
+//A timestamp in JavaScript is a single number that represents a specific point in time.
+let timeStamp = Date.now();
+// console.log(timeStamp);
+// console.log(myDate.getTime());//concept for Bnb websites
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
